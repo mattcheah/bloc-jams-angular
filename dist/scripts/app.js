94224ff1928cc0c794e5275d53809bbca7a94c29
Matt C <matt.cheah@gmail.com>
2017-02-08 14:08:45 -0800

    create controller for album page and add all album information

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -15,7 +15,8 @@
             })
             .state('album', {
                 url: '/album',
-                templateUrl: '/templates/album.html'
+                templateUrl: '/templates/album.html',
+                controller: 'AlbumCtrl as album'
             })
             .state('collection', {
                 url: '/collection',
